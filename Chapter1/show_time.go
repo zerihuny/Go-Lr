9dd098f0c1575868e66e406be6438d03e5fb804f
Zerihuny <zerisht.444@gmail.com>
2025-08-03 23:51:58 +0300

    zz

--- Chapter1/show_time.go
+++ Chapter1/show_time.go
@@ -1,4 +1,20 @@
-// Note: The displayTime function is defined in this file. 
-// To see it in action, uncomment the main function below.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func displayTime() {
+	fmt.Println(time.Now())
+}
+
+// Note: The displayTime function is commented out in main.go to avoid compilation errors
+// when running the program, as it is not defined in main.go.
+//func main() {	
+//	fmt.Println("Hello, world!")
+//	displayTime()
+// }
+// Uncomment the above main function to see the displayTime function in action.
 // The displayTime function prints the current time to the console.
-// This function demonstrates how to work with time in Go.
+// This function can be used to demonstrate how to work with time in Go.	
