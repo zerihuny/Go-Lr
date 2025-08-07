e5a43d44f0121176d64791820ebac4cda7bf5eb7
Zerihuny <zerisht.444@gmail.com>
2025-08-07 14:34:42 +0300

    intance

--- Chapter1/main.go
+++ Chapter1/main.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 )
 
-var num1	 = 42    // type inference
+var num1 = 42    // type inference
 var raining bool // explicit type declaration
 var num2 int
+var num3 = 3.14   // type inference
+var num4 = 1 + 2i // complex number
+var num5 = 1.618  // float64
+var num6 float32
+
+
+
 
 func main() {
 	fmt.Println("Hello, world!")
 	displayTime()
-	fmt.Println(num1 + num2)
+	fmt.Println(num1 + num2 + int(num3) + int(num5) + int(num6))
 	fmt.Println(raining)
+	fmt.Println(num4)
 }
