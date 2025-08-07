3ee49568c36a02b197e5cff12b1d1c1aa1e5260b
Zerihuny <zerisht.444@gmail.com>
2025-08-07 13:58:14 +0300

    remed

--- Chapter1/show_time.go
+++ Chapter1/show_time.go
@@ -8,6 +8,3 @@ import (
 func displayTime() {
 	fmt.Println(time.Now())
 }
-
-// Note: The displayTime function is commented out in main.go to avoid compilation errors
-// when running the program, as it is not defined in main.go.
