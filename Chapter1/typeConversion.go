b1e47af35342b5c50a2085e63700d0ae43e4f3b0
Zerihuny <zerisht.444@gmail.com>
2025-08-11 11:22:36 +0300

    fmt

--- Chapter1/typeConversion.go
+++ Chapter1/typeConversion.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func bini() {
+func main() {
 
-	var age int
+	var input string
 	fmt.Print("Please enter your age: ")
-	fmt.Scanf("%d", &age)
-	fmt.Println("You entered:", age)
+	fmt.Scanf("%s", &input)
+	// fmt.Println("You entered:", age)
+	age, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Error converting age:", err)
+	} else {
+		fmt.Println("Converted age:", age)
+	}
 }
