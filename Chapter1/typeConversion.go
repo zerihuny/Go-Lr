31ceb64ac8859e7c78bf630c8019170eea197a43
zerihunny <zerisht.444@gmail.com>
2025-08-13 06:14:17 +0300

    cmtd

--- Chapter1/typeConversion.go
+++ Chapter1/typeConversion.go
@@ -8,13 +8,13 @@ import (
 func main() {
 
 	var input string
-	fmt.Println("Please enter your age: ")
+	fmt.Print("Please enter your age: ")
 	fmt.Scanf("%s", &input)
-	// fmt.Println("You entered:", age)
+	// fmt.Println("You entered:", input)
 	age, err := strconv.Atoi(input)
-	if err != nil {
-		fmt.Println("Error converting age:", err)
+	if err != nil { // an error occurred
+		fmt.Println(err)
 	} else {
-		fmt.Println("Converted age:", age)
+		fmt.Println("Your age is:", age)
 	}
 }
