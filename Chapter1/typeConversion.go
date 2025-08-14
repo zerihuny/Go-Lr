1ac03e110fdd7e174ebdeaf5df5ec62511359d98
zerihunny <zerisht.444@gmail.com>
2025-08-14 23:48:30 +0300

    fmt

--- Chapter1/typeConversion.go
+++ Chapter1/typeConversion.go
@@ -46,3 +46,18 @@ func yetAnotherOne() {
 		fmt.Println("Your age is:", age)
 	}
 }
+
+// another function to demonstrate type conversion
+func typeConversionDemo() {
+
+	var input string
+	fmt.Print("Please enter your age: ")
+	fmt.Scanf("%s", &input)
+	// fmt.Println("You entered:", input)
+	age, err := strconv.Atoi(input)
+	if err != nil { // an error occurred
+		fmt.Println(err)
+	} else {
+		fmt.Println("Your age is:", age)
+	}
+}
