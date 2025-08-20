0b1ad9473012c0501fa8121652cd4856bfcb2f18
Zerihuny <zerisht.444@gmail.com>
2025-08-20 23:12:35 +0300

    fmt

--- Chapter1/main.go
+++ Chapter1/main.go
@@ -18,16 +18,6 @@ var num4 = 1 + 2i // complex number
 var num5 = 1.618  // float64
 var num6 float32
 
-// firstName := "John" // short variable declaration
-// lastName := "Doe"   // short variable declaration
-// Note: The above short variable declarations will not work at package level in Go.
-// They should be used inside a function or a method.
-// var fullName = firstName + " " + lastName // concatenation
-// Note: The above concatenation will not work at package level in Go.
-// It should be done inside a function or a method.
-// firstName, lastName, age := "John", "Doe", 30 // multiple short variable declarations
-// Note: The above multiple short variable declarations will not work at package level in Go.
-// They should be used inside a function or a method.
 func main() {
 	fmt.Println("Hello, world!")
 	displayTime()
