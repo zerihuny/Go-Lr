ff6a6f4a87a17716f30b48a8ddbeb3d14937de66
Zerihuny <zerisht.444@gmail.com>
2025-09-06 13:19:49 +0300

    fmt

---
+++ chapter3/another.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	score := 72
+	if score >= 70 {
+		fmt.Println("You passed!")
+	} else {
+		fmt.Println("You failed.")
+	}
+}
--- chapter3/decision.go
+++ chapter3/decision.go
@@ -1,30 +1,30 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	result := DecisionExample(85) // Returns "B"
-	fmt.Println(result)
-	logicals()
+// func main() {
+// 	result := DecisionExample(85) // Returns "B"
+// 	fmt.Println(result)
+// 	logicals()
 
-}
+// }
 
-func DecisionExample(score int) string {
-	if score >= 90 {
-		return "A"
-	} else if score >= 80 {
-		return "B"
-	} else if score >= 70 {
-		return "C"
-	} else if score >= 60 {
-		return "D"
-	} else {
-		return "F"
-	}
-}
-func logicals() {
-	num := 10
-	condition := num > 3 && num < 29
-	fmt.Println(condition)
-	// import "fmt"
-}
+// func DecisionExample(score int) string {
+// 	if score >= 90 {
+// 		return "A"
+// 	} else if score >= 80 {
+// 		return "B"
+// 	} else if score >= 70 {
+// 		return "C"
+// 	} else if score >= 60 {
+// 		return "D"
+// 	} else {
+// 		return "F"
+// 	}
+// }
+// func logicals() {
+// 	num := 10
+// 	condition := num > 3 && num < 29
+// 	fmt.Println(condition)
+// 	// import "fmt"
+// }
